df4d490d590a063449f346911744c388ea827152
Aboubacar Kanadji <kaab0601@gmail.com>
2018-09-30 01:21:06 -0500

    save file to plist

--- Todoey/Controller/TodoListViewController.swift
+++ Todoey/Controller/TodoListViewController.swift
@@ -9,19 +9,22 @@
 import UIKit
 
 class TodoListViewController: UITableViewController {
-
-    var itemArray = ["By Eggs", "Buy some meat", "Buy some fish"]
-    let defaults = UserDefaults.standard
+    
+    var itemArray = [Item]()
+      let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Item.plist")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        if let item = self.defaults.array(forKey: "NewItemEntered") as? [String] {
-        itemArray = item
-        }
+        loadData()
+        
+  
+       // print(dataFilePath)
+        
     }
     
     //MARK - TableView Datasource Methods
-   
+    
     //TODO: Declare numberOfRowsInSection here:
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
@@ -30,7 +33,10 @@ class TodoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-        cell.textLabel?.text = itemArray[indexPath.row]
+        cell.textLabel?.text = itemArray[indexPath.row].title
+        
+        cell.accessoryType = itemArray[indexPath.row].done ? .checkmark: .none
+        
         
         return cell
         
@@ -39,14 +45,9 @@ class TodoListViewController: UITableViewController {
     //MARK - TableView Delegate Methods
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-        if(tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark)
-        {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        }
-        else
-        {
-          tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        tableView.reloadData()
+        self.saveData()
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
@@ -55,20 +56,58 @@ class TodoListViewController: UITableViewController {
         
         let alert = UIAlertController(title: "Enter a New Item", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
-            self.itemArray.append(NewItemEntered.text!)
+            let newItemPresent = Item()
+            newItemPresent.title = NewItemEntered.text!
+            self.itemArray.append(newItemPresent)
             self.tableView.reloadData()
-            self.defaults.set(self.itemArray, forKey: "NewItemEntered")
+            self.saveData()
+    
         }
         alert.addTextField { (alertTexField) in
             alertTexField.placeholder = "Enter a New Item"
             NewItemEntered = alertTexField
-    
+            
         }
         
         alert.addAction(action)
         
         present(alert, animated: true, completion: nil)
     }
+    
+    /* function to save data to the plist database */
+    func saveData()
+    {
+        let encoder = PropertyListEncoder()
+        
+        do
+        {
+            let data = try encoder.encode(self.itemArray)
+            try data.write(to: self.dataFilePath!)
+        }catch{
+            
+        }
+    }
+    /* End of save data function*/
+    
+    
+    /* function to load data from the plist database */
+    func loadData()
+    {
+       if let data = try? Data(contentsOf: dataFilePath!)
+       {
+        
+            let decoder = PropertyListDecoder()
+        
+            do
+            {
+                itemArray = try decoder.decode([Item].self, from: data)
+            }catch{
+            
+            }
+       }
+    }
+    
+    /* End of Load data function*/
     
 }
 
---
+++ Todoey/Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Aboubacar Kanadji on 9/29/18.
+//  Copyright © 2018 Aboubacar. All rights reserved.
+//
+
+import Foundation
+
+class Item:Codable {
+    var title: String = ""
+    var done: Bool = false
+}
